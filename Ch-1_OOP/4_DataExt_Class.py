9b69357ad312cb856315e0da178c161a582e1bc0
div9ya <div2020123@gmail.com>
2026-05-24 16:33:42 +0530

    4_DataExt class added

--- Ch-1_OOP/4_DataExt_Class.py
+++ Ch-1_OOP/4_DataExt_Class.py
@@ -1,3 +1,26 @@
+import pandas as pd
+
 class DataExt:
 
-    
+    def __init__(self,file_path:str):
+        self.file_path=file_path
+
+    def fetch_text(self,seperator:str):    
+        #Write your custom logic
+        df=pd.read_csv(self.file_path,sep=seperator)
+        print(df.head())
+
+    def fetch_json(self):    
+        #Write your custom logic
+        df=pd.read_json(self.file_path)
+        print(df.head())    
+
+    def fetch_parquet(self):    
+        #Write your custom logic
+        df=pd.read_parquet(self.file_path)
+        print(df.head())
+
+
+obj=DataExt("Ch-1_OOP\Files\orders.json")
+# obj.fetch_text("\t")
+obj.fetch_json()
