41875e56b507293c14a1495ff92bf0aa229790ad
div9ya <div2020123@gmail.com>
2026-05-24 15:41:34 +0530

    1_basics added

---
+++ Ch-1_OOP/1_basics.py
@@ -0,0 +1,14 @@
+class MyClass:
+    var1="Divya"
+    var2="Verma"
+
+    def func1(self):
+        print("Hello World")
+
+    def func2(self):
+        print("Hello Globe")
+
+
+obj=MyClass()
+obj.func1()
+obj.func2()
